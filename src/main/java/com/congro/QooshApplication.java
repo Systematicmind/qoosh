65f17901e85e8bc1a209db6b0f392baa1bdbfcae
Amir Hajizadeh <amir.account@gmail.com>
2016-12-17 23:21:49 +0330

    first commit

---
+++ src/main/java/com/congro/QooshApplication.java
@@ -0,0 +1,12 @@
+package com.congro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QooshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QooshApplication.class, args);
+	}
+}
---
+++ src/test/java/com/congro/QooshApplicationTests.java
@@ -0,0 +1,16 @@
+package com.congro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QooshApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
